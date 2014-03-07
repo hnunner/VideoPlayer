b7e476371f597a1486152ea2441f03dad4d954ae
Hendrik Nunner <hendrik.nunner@securizon.com>
2014-03-07 13:44:56 +0100

    added web view inlcuding cookie reader functionalities

--- VideoPlayer/VideoPlayerViewController.m
+++ VideoPlayer/VideoPlayerViewController.m
@@ -9,9 +9,10 @@
 #import "VideoPlayerViewController.h"
 #import <MediaPlayer/MediaPlayer.h>
 
-@interface VideoPlayerViewController ()
+@interface VideoPlayerViewController () <UIWebViewDelegate>
 @property (weak, nonatomic) IBOutlet UIView *moviePlayerView;
 @property (nonatomic, strong) MPMoviePlayerController *moviePlayerController;
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
 
@@ -21,7 +22,7 @@
 - (MPMoviePlayerController *) moviePlayerController {
     if (!_moviePlayerController) {
         _moviePlayerController = [[MPMoviePlayerController alloc] init];
-        [self prepareMp4Movie:@"short"];
+        [self prepareLocalMp4Movie:@"short"];
     }
     return _moviePlayerController;
 }
@@ -35,7 +36,7 @@
     // layout
     {
         self.view.backgroundColor = [UIColor darkGrayColor];
-        self.moviePlayerView.frame = CGRectMake(40, 40, 240, 240);
+        self.moviePlayerView.frame = CGRectMake(80, 20, 180, 180);
     }
     
     // movie player
@@ -52,6 +53,20 @@
         
         // trying to repeat
         self.moviePlayerController.repeatMode = MPMovieRepeatModeOne;
+    }
+    
+    // web view
+    {
+        // layout and size
+        self.webView.scalesPageToFit = YES;
+        
+        
+        // load google website
+        self.webView.delegate = self;
+        NSString *googlePath = @"http://www.google.de";
+        NSURL *googleUrl = [NSURL URLWithString:googlePath];
+        NSURLRequest *googleURLRequest = [[NSURLRequest alloc] initWithURL:googleUrl];
+        [self.webView loadRequest:googleURLRequest];
     }
 }
 
@@ -79,19 +94,70 @@
     NSString *videoPath = self.moviePlayerController.contentURL.description;
     NSRange rangeOfTitle = [videoPath rangeOfString:@"short"];
     if (rangeOfTitle.length) {
-        [self prepareMp4Movie:@"car"];
+        [self prepareLocalMp4Movie:@"car"];
     }
     else {
-        [self prepareMp4Movie:@"short"];
+        [self prepareLocalMp4Movie:@"short"];
     }
 }
 
 
 #pragma mark - Custom Logic
 
-- (void)prepareMp4Movie:(NSString *) movie {
+- (void)prepareRemoteMp4Movie:(NSString *) movieName {
+    NSString *movieFilePath = [[NSString alloc] initWithFormat:@"http://localhost/dump/%@.mp4", movieName];
+    NSLog(@"Loading file from: %@", movieFilePath);
+    
+    NSURL *mp4MovieFileURL = [NSURL URLWithString:movieFilePath];
+    self.moviePlayerController.contentURL = mp4MovieFileURL;
+}
+
+- (void)prepareLocalMp4Movie:(NSString *) movie {
     NSURL *mp4MovieURL = [[NSBundle mainBundle] URLForResource:movie withExtension:@"mp4"];
     self.moviePlayerController.contentURL = mp4MovieURL;
+}
+
+- (void) readCookies {
+    NSHTTPCookie *cookie;
+    NSHTTPCookieStorage *cookieJar = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+    for (cookie in [cookieJar cookies]) {
+        NSLog(@"Cookie comment: %@", [cookie comment]);
+        NSLog(@"Cookie comment URL: %@", [cookie commentURL]);
+        NSLog(@"Cookie domain: %@", [cookie domain]);
+        NSLog(@"Cookie expires date: %@", [cookie expiresDate]);
+        NSLog(@"Cookie is HTTP only: %c", [cookie isHTTPOnly]);
+        NSLog(@"Cookie is secure: %c", [cookie isSecure]);
+        NSLog(@"Cookie is session only: %c", [cookie isSessionOnly]);
+        NSLog(@"Cookie name: %@", [cookie name]);
+        NSLog(@"Cookie path: %@", [cookie path]);
+        NSLog(@"Cookie port list: %@", [cookie portList]);
+        NSLog(@"Cookie properties: %@", [cookie properties]);
+        NSLog(@"Cookie value: %@", [cookie value]);
+        NSLog(@"Cookie version: %d", [cookie version]);
+    }
+    
+    NSDictionary * headers = [NSHTTPCookie requestHeaderFieldsWithCookies:
+                              [cookieJar cookies]];
+    NSUInteger headerCount = [headers count];
+    NSLog(@"Cookie name: %@", [headers objectForKey:NSHTTPCookieName]);
+    NSLog(@"Cookie headers count: %i", headerCount);
+    for (NSString *key in headers) {
+        NSLog(@"Cookie Header: %@=%@", key, [headers objectForKey:key]);
+    }
+}
+
+- (void)addCookiesToRequest:(NSMutableURLRequest *) request {
+    NSHTTPCookieStorage *cookieJar = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+    NSDictionary * headers = [NSHTTPCookie requestHeaderFieldsWithCookies:
+                              [cookieJar cookies]];
+    [request setAllHTTPHeaderFields:headers];
+}
+
+# pragma mark - UIWebViewDelegate
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    NSLog(@"Web view finished loading.. Start reading cookie ;-)");
+    [self readCookies];
 }
 
 @end
